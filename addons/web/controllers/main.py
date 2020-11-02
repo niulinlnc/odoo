e440ddba82bb40592fbb451054594d268a71101f
banqu <banqu@163.com>
2020-11-02 23:06:42 +0800

    up

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -2068,6 +2068,7 @@ class ReportController(http.Controller):
         type [1]
         :returns: Response with a filetoken cookie and an attachment header
         """
+        print('>>>>>>>>>>>>>>>>>>>data',data)
         requestcontent = json.loads(data)
         url, type = requestcontent[0], requestcontent[1]
         try:
